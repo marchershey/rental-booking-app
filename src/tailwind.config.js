5b93e1ca0b49cca0afb324f36486e01936f9a46f
Marc Hershey <marchershey@noreply.users.github.com>
2022-01-25 01:34:28 +0000

    npm: installed @tailwindcss/line-clamp

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -30,6 +30,8 @@ module.exports = {
     plugins: [
         require('@tailwindcss/forms'),
         require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/line-clamp'),
+
 
         // ...
     ],
