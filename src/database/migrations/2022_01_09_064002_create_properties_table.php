9bfe08333b336ff6da23b621cbfda23302540459
Marc Hershey <marchershey@noreply.users.github.com>
2022-01-26 21:33:41 +0000

    disallow properties ratings to be null

--- src/database/migrations/2022_01_09_064002_create_properties_table.php
+++ src/database/migrations/2022_01_09_064002_create_properties_table.php
@@ -27,8 +27,8 @@ class CreatePropertiesTable extends Migration
             $table->float('bathrooms', 3, 1)->nullable();
             $table->string('listing_headline')->nullable();
             $table->text('listing_desc')->nullable();
-            $table->string('listing_rating')->nullable()->default('0.0');
-            $table->string('listing_rating_count')->nullable()->default('0');
+            $table->string('listing_rating')->default('0.0');
+            $table->string('listing_rating_count')->default('0');
             $table->boolean('visible')->default(true)->nullable();
             $table->boolean('deleted')->default(false)->nullable();
             $table->integer('user_id')->nullable();
